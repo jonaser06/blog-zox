fb3f19fb3657d8e100d485f1fa06cf10e98754f6
Jonathan Narvaez <jonaser06@gmail.com>
2020-01-21 23:56:01 -0500

    test

--- js/main.js
+++ js/main.js
@@ -35,9 +35,23 @@ var objBlog = {
         $(window).on("scroll", function(){
             if($(window).scrollTop() > altura ){
                 $(".header").addClass("fixedheader");
+                $(".sidebar").addClass("sidebar-fixed");
+                var ancho = $(".interna-content").width();
+                ancho = ancho*0.3;
+                $(".sidebar-fixed").width(ancho-60);
             }else{
                 $(".header").removeClass("fixedheader");
+                $(".sidebar").removeClass("sidebar-fixed");
+                var ancho = $(".interna-content").width();
+                ancho = ancho*0.3;
+                $(".sidebar").width(ancho-60);
             }
+        });
+
+        $(window).resize(function(){
+            var ancho = $(".interna-content").width();
+            ancho = ancho*0.3;
+            $(".sidebar-fixed").width(ancho);
         });
     }
 }
